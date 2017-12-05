c5139e9927ea9cde5aac778424868f19f41f430d
hellofromtonya <hellofromtonya@knowthecode.io>
2017-12-04 21:03:47 -0600

    Adding TravisCI

---
+++ bin/install-phpunit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir -p $HOME/phpunit-bin
+
+if [[ ${TRAVIS_PHP_VERSION:0:2} == "7." ]]; then
+  PHPUNIT_VERSION='5.7'
+elif [[ ${TRAVIS_PHP_VERSION:0:3} != "5.2" ]]; then
+  PHPUNIT_VERSION='4.8'
+fi
+
+composer global require "phpunit/phpunit=$PHPUNIT_VERSION"
+
+export PATH=$HOME/phpunit-bin/:$PATH
